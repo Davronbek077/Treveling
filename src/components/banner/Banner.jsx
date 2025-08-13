9597eb03e2c87ea64b4738b5b123c50e4b101ed1
wwecoder15 <rahmonovdavronbek770@gmail.com>
2025-08-13 10:34:27 +0500

    video changed

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -387,7 +387,7 @@ const Banner = () => {
             <iframe
       width="100%"
       height="455"
-      src="https://www.youtube.com/embed/VFJPYlqpgeU?si=0ChtKvKkjh8tFe7d"
+      src="https://www.youtube.com/embed/XeAiIWaeT-o?si=avAmFM2ZFbm18dle"
       title="YouTube video player"
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
